649f5705afcdfc35ce7af977e3910d61f22676b1
ericrosado <erosado@8thlight.com>
2018-01-19 15:25:27 -0600

    Server returns 'Hello World' upon request.

--- Sources/SwiftServer/main.swift
+++ Sources/SwiftServer/main.swift
@@ -1,1 +1,25 @@
+import Foundation
+import Socket
 
+class Server {
+    
+    private let port: Int
+    private let acceptNewConnection = true
+
+    init(port: Int){
+        self.port = port
+    }
+
+    public func createSocket() throws{
+        let socket = try Socket.create()
+        try socket.listen(on: port)
+        repeat {
+            let connectedSocket = try socket.acceptClientConnection()
+            try connectedSocket.write(from: "Hello World\n")
+        } while acceptNewConnection
+        
+    }
+}
+    
+let server = Server(port: 3333)
+try server.createSocket()
