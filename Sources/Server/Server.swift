18fb9f2b16dbf9fbe834767ed08f98d2540dcdb3
ericrosado <erosado@8thlight.com>
2018-01-29 10:53:20 -0600

    GET and HEAD requests fulfilled with conditional.

--- Sources/Server/Server.swift
+++ Sources/Server/Server.swift
@@ -5,38 +5,38 @@ public class Server {
     
     private let port: Int
     private let acceptNewConnection = true
-    private let requestBody: [String: String] = ["GET": "<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>", "HEAD": ""]
-
+    static let body = "<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>"
+    static let header = "HTTP/1.1 200 OK\r\nContent-Length: \(body.utf8.count)\r\nContent-type: text/html\r\n\r\n"
+    
     public init(port: Int){
         self.port = port
     }
 
-    public func startServer() throws{
+    public func startServer() throws {
         let socket = try Socket.create()
         try socket.listen(on: port)
         repeat {
             let connectedSocket = try socket.acceptClientConnection()
-            let request = try connectedSocket.readString() 
-            let httpRequest = parseRequest(request: request!)
-            try connectedSocket.write(from: httpRequest.request)
+            let request = try connectedSocket.readString()! 
+            let httpRequest = parseRequest(request: request)
+            let response = prepareResponse(request: httpRequest)
+            try connectedSocket.write(from: response)
             connectedSocket.close()
         } while acceptNewConnection
     }
 
-    private func parseRequest(request: String) -> Request{
-        let requestMethod = request.components(separatedBy: " /")
-        return  Request(body: requestBody[requestMethod[0]]!)
+    public func parseRequest(request: String) -> String {
+        let requestMethod = request.components(separatedBy: " ")
+        return requestMethod[0]
+    }
+
+    public func prepareResponse(request: String) -> String {
+        if (request == "GET") {
+           return Server.header + Server.body          
+        } else {
+            return Server.header
+        } 
     }
 
 }
 
-struct Request {
-
-    let request: String
-    
-    init(body: String) {
-       let body = body
-       let header = "HTTP/1.1 200 OK\r\nContent-Length: \(body.utf8.count)\r\nContent-type: text/html\r\n\r\n"
-       request = header + body 
-    }
-}
