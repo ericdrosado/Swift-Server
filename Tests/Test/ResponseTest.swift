49f19198b902ce189538a8bccc43ea6aa34fcdc9
ericrosado <erosado@8thlight.com>
2018-01-31 13:10:47 -0600

    Updated tests

--- Tests/Test/ResponseTest.swift
+++ Tests/Test/ResponseTest.swift
@@ -4,30 +4,34 @@ import Server
 class ResponseTest: XCTestCase {
     
     let response = Response()
-    static let body = "<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>"
-    static let header = "HTTP/1.1 200 OK\r\nContent-Length: \(body.utf8.count)\r\nContent-type: text/html\r\n\r\n"
+    static let body200 = "<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>"
+    static let body404 = "<!DOCTYPE html><html><body><h1>404 Page Not Found</h1></body></html>"
+    let header200 = "HTTP/1.1 200 OK\r\nContent-Length: \(body200.utf8.count)\r\nContent-type: text/html\r\n\r\n"
+    let header404 = "HTTP/1.1 404 Not Found\r\nContent-Length: \(body404.utf8.count)\r\nContent-type: text/html\r\n\r\n"
+    let header200HEAD = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\nContent-type: text/html\r\n\r\n"
+    let header404HEAD = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\nContent-type: text/html\r\n\r\n"
 
     func testBuildResponseWillReturnGETResponse() {
         let request = "GET / "  
-        let expectedResponse = response.header200 + Response.body            
+        let expectedResponse = header200 + ResponseTest.body200            
         XCTAssertEqual(expectedResponse, response.buildResponse(serverRequest: request))
     }
 
     func testBuildResponseWillReturnHEADResponse() {
         let request = "HEAD / " 
-        let expectedResponse = response.header200 
+        let expectedResponse = header200HEAD 
         XCTAssertEqual(expectedResponse, response.buildResponse(serverRequest: request))
     }
 
     func testBuildResponseWillReturn404ResponseWithGET() {
         let request = "GET /test " 
-        let expectedResponse = response.header404            
+        let expectedResponse = header404 + ResponseTest.body404            
         XCTAssertEqual(expectedResponse, response.buildResponse(serverRequest: request))
     }
 
     func testBuildResponseWillReturn404ResponseWithHEAD() {
         let request = "HEAD /test " 
-        let expectedResponse = response.header404 
+        let expectedResponse = header404HEAD 
         XCTAssertEqual(expectedResponse, response.buildResponse(serverRequest: request))
     }
 
