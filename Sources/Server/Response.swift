8d944cc283efa3ae91f2a15b06e0ceb93a9d3778
ericrosado <erosado@8thlight.com>
2018-01-31 13:05:14 -0600

    Added parsing for queries with the ability to make a noun query.

--- Sources/Server/Response.swift
+++ Sources/Server/Response.swift
@@ -1,41 +1,60 @@
- import Foundation
+import Foundation
 
- public class Response {
+public class Response {
 
-    public static let body = "<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>"
-    public let header404 = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\nContent-type: text/html\r\n\r\n" 
-    public let header200 = "HTTP/1.1 200 OK\r\nContent-Length: \(body.utf8.count)\r\nContent-type: text/html\r\n\r\n" 
+   public var queries: [String:String] = ["noun": "World"]
+   public var paths = ["/","/hello"]
+   public let status200 = "200 OK"
+   public let status404 = "404 Not Found"
+   public let status404Body = "404 Page Not Found"
 
-    public init(){}
+   public init(){}
 
-    public func buildResponse(serverRequest: String) -> String {
-        let (requestMethod, requestPath) = parseRequest(request: serverRequest)
-        let responseHeader = prepareHeader(path: requestPath)
-        return  prepareResponse(method: requestMethod, header: responseHeader) 
-    }
+   public func buildResponse(serverRequest: String) -> String {
+       let (requestMethod, requestPath) = parseRequest(request: serverRequest)
+       return  prepareResponse(method: requestMethod, path: requestPath)
+   }
 
-    private func parseRequest(request: String) -> (String, String) {
+   private func prepareResponse(method: String, path: String) -> String {
+       let body: String
+       let header: String
+       if (paths.contains(path)) {
+           body = prepareBody(body: "Hello \(queries["noun"]!)", method: method)
+           header = buildHeader(statusCode: status200, contentLength: body.utf8.count) 
+       } else {
+           body = prepareBody(body: status404Body, method: method) 
+           header = buildHeader(statusCode: status404, contentLength: body.utf8.count) 
+       }      
+       return header + body
+   }
+
+   private func prepareBody(body: String, method: String) -> String {
+       if (body == status404Body && method == "HEAD" || method == "HEAD") {
+           return ""    
+       } else {
+           return "<!DOCTYPE html><html><body><h1>\(body)</h1></body></html>"
+       }
+   }
+
+   private func buildHeader(statusCode: String, contentLength: Int) -> String {
+      return "HTTP/1.1 \(statusCode)\r\nContent-Length: \(contentLength)\r\nContent-type: text/html\r\n\r\n" 
+   }
+
+   private func parseRequest(request: String) -> (String, String) {
         let requestComponent = request.components(separatedBy: " ")
+            if (requestComponent[1].contains("?")) {
+                let path = parsePathForQueries(path: requestComponent[1])
+                return (requestComponent[0], path) 
+            }
         return (requestComponent[0], requestComponent[1])
-    }
+   }
 
-    private func prepareHeader(path: String) -> String {
-        if (path != "/") {
-            return header404
-        } else {
-            return header200       
-        }      
-    }
+   private func parsePathForQueries(path: String) -> String {
+        let parsedQueries = path.split(separator: "?") 
+        let query = parsedQueries[1].split(separator: "=") 
+        self.queries.updateValue(String(query[1]), forKey: String(query[0]))
+        return String(parsedQueries[0])
+   }
 
-    private func prepareResponse(method: String, header: String) -> String {
-        if (header == header404) {
-            return header 
-        } else if (method == "GET") {
-            return header + Response.body          
-        } else {
-            return header
-        } 
-    }
-    
 }
 
