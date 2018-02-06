12a683c42c209a86f2ff9e7cb6f8d415ef06d601
ericrosado <erosado@8thlight.com>
2018-02-06 09:43:34 -0600

    Pushed cob_spec changes to forked directory.

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@ swift run &
 PID=$!
 cd cob_spec
 mvn package
-java -jar fitnesse.jar -c 'PassingTestSuite?suite&format=text'
+java -jar fitnesse.jar -c "PassingTestSuite?suite&format=text"
 kill -9 $PID
