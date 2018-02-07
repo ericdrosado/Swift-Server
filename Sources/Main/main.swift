836298bd1cce40009d832657aecb658236a45af3
ericrosado <erosado@8thlight.com>
2018-02-07 14:48:08 -0600

    Added new feature in main.swift where user can insert a port number on command line. Updated README based on new feature

--- Sources/Main/main.swift
+++ Sources/Main/main.swift
@@ -2,5 +2,11 @@ import Server
 
 let parser = Parser()
 let response = Response(parser: parser)
-let server = Server(port: 5000, response: response)
-try server.startServer()
+
+if CommandLine.argc < 2 {
+    let server = Server(port: 5000, response: response)
+    try server.startServer()
+} else {
+    let server = Server(port: Int(CommandLine.arguments[1])!, response: response)
+    try server.startServer()
+}
