7fc6419b91ad6d0e2b1fa4a91f45894ed852b875
ericrosado <erosado@8thlight.com>
2018-02-14 14:44:50 -0600

    Introduced directory flag for CLIUtility class.

--- Sources/Main/CLIUtility.swift
+++ Sources/Main/CLIUtility.swift
@@ -6,12 +6,14 @@ public class CLIUtility {
     let arguments: [String] 
     let parser: ArgumentParser 
     let port: OptionArgument<Int>
+    let directory: OptionArgument<String>
     let parsedArguments: ArgumentParser.Result 
 
     init() throws {
         arguments = Array(ProcessInfo.processInfo.arguments.dropFirst())
         parser = ArgumentParser(usage: "<options>", overview: "Please use one of the following options when necessary")
         port = parser.add(option: "--port", shortName: "-p", kind: Int.self, usage: "Enter a port to listen on")
+        directory = parser.add(option: "--directory", shortName: "-d", kind: String.self, usage: "Enter a directory")
         parsedArguments = try parser.parse(arguments)
     }
 
