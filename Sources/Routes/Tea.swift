149d543d48718c1bdf91f13e3bcc95083a2805b6
ericrosado <erosado@8thlight.com>
2018-06-11 13:16:02 -0500

    Update Tea route with enum for status line.

--- Sources/Routes/Tea.swift
+++ Sources/Routes/Tea.swift
@@ -8,7 +8,7 @@ public class Tea: Route {
 
     public func handleRoute(request: Request) -> ResponseData {
         let body = "" 
-        return ResponseData(statusLine: Status.status200(version: request.httpVersion), 
+        return ResponseData(statusLine: HTTPStatus.ok.toStatusLine(version: request.httpVersion), 
                             headers: Headers().getHeaders(body: body, route: request.path), 
                             body: body)        }
 
